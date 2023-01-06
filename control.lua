ec058f386026b248c45210e5fcbf2ad6811125d2
Spacedestructor <Spacedestructor@users.noreply.github.com>
2023-01-06 18:12:09 +0100

    Big Overhaul
    Fixed: Mod now only runs in multiplayer with a notification in singleplayer that the mod wont run.
    Fixed: when attempting to use commands the mod will inform the player that its commands can only be used by the System.
    Fixed: Mod now propperly parses the player name from the channelname.
    Fixed: Mod now uses default values if not all Parameters where supplied, channelname will always be a hard requirement.
    I might have forgotten a few changes if this list is incomplete.

--- control.lua
+++ control.lua
@@ -1,104 +1,232 @@
-commands.add_command("SpawnPollution",nil,function(command)
-	if command.player_index == nil then
-		local player = gui.player
-		local parameter = tonumber(command.parameter) --first is the amount of pollution, second is the name who to target.
-		player.surface.pollute(player.position, parameter)
+local SFSI = {
+	ChannelName = {}, --player name is the key, channel name the value.
+	SpawnPollutionEnable = {}, --player name is the key, settings value the value.
+	SpawnPollutionAmmount = {}, --player name is the key, settings value the value.
+	TeleportEnable = {}, --player name is the key, settings value the value.
+	TeleportRadius = {}, --player name is the key, settings value the value.
+	ParanoiaEnable = {}, --player name is the key, settings value the value.
+	ParanoiaRadius = {}, --player name is the key, settings value the value.
+	ParanoiaLimit = {}, --player name is the key, settings value the value.
+	AttackEnable = {}, --player name is the key, settings value the value.
+	AttackLimit = {} --player name is the key, settings value the value.
+}
+function ParseName(channelname)
+	for key, value in pairs(SFSI.ChannelName) do
+		if value == channelname then
+			return key
+		end
+	end
+end
+script.on_event(defines.events.on_player_joined_game, function(event) --player_index, name, tick.
+	if game.is_multiplayer() then
+		local player = game.get_player(event.player_index)
+		local player_name = player.name
+		SFSI.ChannelName[player_name] = game.mod_setting_prototypes["ChannelName"]
+		SFSI.SpawnPollutionEnable[player_name] = game.mod_setting_prototypes["SpawnPollution-Enable"]
+		SFSI.SpawnPollutionAmmount[player_name] = game.mod_setting_prototypes["SpawnPollution-Pollution"]
+		SFSI.TeleportEnable[player_name] = game.mod_setting_prototypes["Teleport-Enable"]
+		SFSI.TeleportRadius[player_name] = game.mod_setting_prototypes["Teleport-Radius"]
+		SFSI.ParanoiaEnable[player_name] = game.mod_setting_prototypes["Paranoia-Enable"]
+		SFSI.ParanoiaRadius[player_name] = game.mod_setting_prototypes["Paranoia-Radius"]
+		SFSI.ParanoiaLimit[player_name] = game.mod_setting_prototypes["Paranoia-Limit"]
+		SFSI.AttackEnable[player_name] = game.mod_setting_prototypes["Attack-Enable"]
+		SFSI.AttackLimit[player_name] = game.mod_setting_prototypes["Attack-Limit"]
 	else
-		local player = game.get_player(command.player_index)
-		player.print("The Command " .. command.name .. " can only be used by the Stream Integration Mod!", player.chat_color)
+		local player = game.get_player(1)
+		player.print("The Stream Integration Mod is only available in Multiplayer!", player.chat_color)
+	end
+end)
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event) -- player_index, setting, setting_type, name, tick.
+	if game.is_multiplayer() then
+		local player = game.get_player(event.player_index)
+		local player_name = player.name
+		local setting_name = event.setting
+		local setting_value = settings.player[setting_name].value
+		SFSI.ChannelName[player_name] = game.mod_setting_prototypes["ChannelName"]
+		SFSI.SpawnPollutionEnable[player_name] = game.mod_setting_prototypes["SpawnPollution-Enable"]
+		SFSI.SpawnPollutionAmmount[player_name] = game.mod_setting_prototypes["SpawnPollution-Pollution"]
+		SFSI.TeleportEnable[player_name] = game.mod_setting_prototypes["Teleport-Enable"]
+		SFSI.TeleportRadius[player_name] = game.mod_setting_prototypes["Teleport-Radius"]
+		SFSI.ParanoiaEnable[player_name] = game.mod_setting_prototypes["Paranoia-Enable"]
+		SFSI.ParanoiaRadius[player_name] = game.mod_setting_prototypes["Paranoia-Radius"]
+		SFSI.ParanoiaLimit[player_name] = game.mod_setting_prototypes["Paranoia-Limit"]
+		SFSI.AttackEnable[player_name] = game.mod_setting_prototypes["Attack-Enable"]
+		SFSI.AttackLimit[player_name] = game.mod_setting_prototypes["Attack-Limit"]
+	else
+		local player = game.get_player(1)
+		player.print("The Stream Integration Mod is only available in Multiplayer!", player.chat_color)
+	end
+end)
+script.on_event(defines.events.on_player_created, function(event) --player_index, name, tick.
+	if game.is_multiplayer() then
+		local player = game.get_player(event.player_index)
+		local player_name = player.name
+		SFSI.ChannelName[player_name] = game.mod_setting_prototypes["ChannelName"]
+		SFSI.SpawnPollutionEnable[player_name] = game.mod_setting_prototypes["SpawnPollution-Enable"]
+		SFSI.SpawnPollutionAmmount[player_name] = game.mod_setting_prototypes["SpawnPollution-Pollution"]
+		SFSI.TeleportEnable[player_name] = game.mod_setting_prototypes["Teleport-Enable"]
+		SFSI.TeleportRadius[player_name] = game.mod_setting_prototypes["Teleport-Radius"]
+		SFSI.ParanoiaEnable[player_name] = game.mod_setting_prototypes["Paranoia-Enable"]
+		SFSI.ParanoiaRadius[player_name] = game.mod_setting_prototypes["Paranoia-Radius"]
+		SFSI.ParanoiaLimit[player_name] = game.mod_setting_prototypes["Paranoia-Limit"]
+		SFSI.AttackEnable[player_name] = game.mod_setting_prototypes["Attack-Enable"]
+		SFSI.AttackLimit[player_name] = game.mod_setting_prototypes["Attack-Limit"]
+	else
+		local player = game.get_player(1)
+		player.print("The Stream Integration Mod is only available in Multiplayer!", player.chat_color)
+	end
+end)
+commands.add_command("SpawnPollution",nil,function(command)
+	if game.is_multiplayer() then
+		if command.player_index == nil then
+			if SFSI.SpawnPollutionEnable then
+				local parameters = game.json_to_table(command.parameter) --pollution, name.
+				local channelname = parameters["name"]
+				local player = game.get_player(ParseName(channelname))
+				local pollution_default = SFSI.SpawnPollutionAmmount[player]
+				local pollution = parameters["pollution"]
+				if pollution == nil or pollution <= 0 then
+					pollution = pollution_default
+				end
+				player.surface.pollute(player.position, pollution)
+			end
+		else
+			local player = game.get_player(command.player_index)
+			player.print("[Stream Integration] The Command " .. command.name .. " can only be used by the Stream Integration Mod!", player.chat_color)
+		end
+	else
+		local player = game.get_player(1)
+		player.print("[Stream Integration] The Stream Integration Mod is only available in Multiplayer!", player.chat_color)
 	end
 end)
 commands.add_command("Teleport",nil,function(command)
-	if command.player_index == nil then
-		local player = gui.player
-		local parameters = {} --first is the radius, second is the name.
-		for key, value in string.gmatch(command.parameter, "%d") do
-			parameters[key] = value
-		end
-		parameters[1] = tonumber(parameters[1])
-		player.surface.find_non_colliding_position(player.name, player.position, parameters[1], 1, true)
-	else
-		local player = game.get_player(command.player_index)
-		player.print("The Command " .. command.name .. " can only be used by the Stream Integration Mod!", player.chat_color)
-	end
-end)
-command.add_command("Paranoia",nil,function(command)
-	if command.player_index == nil then
-		local player = gui.player
-		local parameters = {} --first is the Radius, second is the Limit, third is the name who to target.
-		for key, value in string.gmatch(command.parameter, "%d") do
-			parameters[key] = value
-		end
-		parameters[1] = tonumber(parameters[1])
-		parameters[2] = tonumber(parameters[2])
-		local entities = player.surface.find_entities_filtered{force=player.force} --Apparently you cant invert the radius or limit filters.
-		for key, value in pairs(entities) do
-			local distance = math.sqrt((value.position.x - player.position.x)^2 + (value.position.y - player.position.y)^2)
-			if distance <= parameters[1] then
-				table.remove(entities, entities[key])
+	if game.is_multiplayer() then
+		if command.player_index == nil then
+			if SFSI.TeleportEnable then
+				local parameters = game.json_to_table(command.parameter) --radius, name.
+				local channelname = parameters["name"]
+				local player = game.get_player(ParseName(channelname))
+				local radius_default = SFSI.TeleportRadius
+				local radius = parameters["radius"]
+				if radius == nil or radius <= 0 then
+					radius = radius_default
+				end
+				local position = player.surface.find_non_colliding_position(player.name, player.position, radius, 1, true)
+				player.teleport(position, player.surface)
 			end
-		end
-		local TableSize = 0
-		for key, value in pairs(entities) do
-			TableSize = TableSize + 1
-		end
-		while TableSize >= parameters[2] do
-			table.remove(entities, math.random(TableSize))
-			TableSize = TableSize - 1
-		end
-		for key, value in pairs(entities) do
-			if entities[key].type == "artillery-turret" or "ammo-turret" or "electric-turret" or "fluid-turret" then
-				player.add_alert(value, defines.alert_type.turret_fire)
-			else
-				player.add_alert(value, defines.alert_type.entity_under_attack)
-			end
+		else
+			local player = game.get_player(command.player_index)
+			player.print("[Stream Integration] The Command " .. command.name .. " can only be used by the Stream Integration Mod!", player.chat_color)
 		end
 	else
-		local player = game.get_player(command.player_index)
-		player.print("The Command " .. command.name .. " can only be used by the Stream Integration Mod!", player.chat_color)
+		local player = game.get_player(1)
+		player.print("[Stream Integration] The Stream Integration Mod is only available in Multiplayer!", player.chat_color)
 	end
 end)
-command.add_command("Attack",nil,function(command) --first is how many biters/spitters should attack, second is the name who to target.
-	local player = gui.player
-	local parameters = {}
-	for key, value in string.gmatch(command.parameter, "%d") do
-		parameters[key] = value
+commands.add_command("Paranoia",nil,function(command)
+	if game.is_multiplayer() then
+		if command.player_index == nil then
+			if SFSI.ParanoiaEnable then
+				local parameters = game.json_to_table(command.parameter) --radius, limit, name.
+				local channelname = parameters["name"]
+				local player = game.get_player(ParseName(channelname))
+				local radius_default = SFSI.ParanoiaRadius
+				local radius = parameters["radius"]
+				if radius == nil or radius <= 0 then
+					radius = radius_default
+				end
+				local limit_default = SFSI.ParanoiaLimit
+				local limit = parameters["limit"]
+				if limit == nil or limit <= 0 then
+					limit = limit_default
+				end
+				local entities = player.surface.find_entities_filtered{force=player.force} --Apparently you cant invert the radius or limit filters.
+				for key, value in pairs(entities) do
+					local distance = math.sqrt((value.position.x - player.position.x)^2 + (value.position.y - player.position.y)^2)
+					if distance <= radius then
+						table.remove(entities, entities[key])
+					end
+				end
+				local TableSize = 0
+				for key, value in pairs(entities) do
+					TableSize = TableSize + 1
+				end
+				while TableSize >= limit do
+					table.remove(entities, math.random(TableSize))
+					TableSize = TableSize - 1
+				end
+				for key, value in pairs(entities) do
+					if entities[key].type == "artillery-turret" or "ammo-turret" or "electric-turret" or "fluid-turret" then
+						player.add_alert(value, defines.alert_type.turret_fire)
+					else
+						player.add_alert(value, defines.alert_type.entity_under_attack)
+					end
+				end
+			end
+		else
+			local player = game.get_player(command.player_index)
+			player.print("[Stream Integration] The Command " .. command.name .. " can only be used by the Stream Integration Mod!", player.chat_color)
+		end
+	else
+		local player = game.get_player(1)
+		player.print("[Stream Integration] The Stream Integration Mod is only available in Multiplayer!", player.chat_color)
 	end
-	parameters[1] = tonumber(parameters[1])
-	local entities = player.surface.find_entities_filtered{type="unit",force="enemy"}
-	local TableSize = 0
-	for key, value in pairs(entities) do
-		TableSize = TableSize + 1
-	end
-	while TableSize > parameters[1] do
-		table.remove(entities, math.random(TableSize))
-		TableSize = TableSize - 1
-	end
-	for key, value in pairs(entities) do
-		value.set_command{
-			type = defines.command.compound,
-			structure_type = defines.compound_command.logical_and,
-			command = {
-				{
-					type = defines.command.go_to_location,
-					destination_entity = player,
-					distraction = defines.distraction.by_anything,
-					pathfind_flags = {
-						allow_destroy_friendly_entities = false,
-						allow_paths_through_own_entities = false,
-						cache = false,
-						prefer_straight_paths = false,
-						low_priority = false,
-						no_break = true
-					},
-					radius = 1
-				},
-				{
-					type = defines.command.attack,
-					target = player,
-					distraction = defines.distraction.by_damage
-				}
-			}
-		}
+end)
+commands.add_command("Attack",nil,function(command)
+	if game.is_multiplayer() then
+		if command.player_index == nil then
+			if SFSI.AttackEnable then
+				local parameters = game.json_to_table(command.parameter) --limit, name.
+				local channelname = parameters["name"]
+				local player = game.get_player(ParseName(channelname))
+				local limit_default = SFSI.AttackLimit
+				local limit = parameters["limit"]
+				if limit == nil or limit <= 0 then
+					limit = limit_default
+				end
+				local entities = player.surface.find_entities_filtered{type="unit",force="enemy"}
+				local TableSize = 0
+				for key, value in pairs(entities) do
+					TableSize = TableSize + 1
+				end
+				while TableSize > limit do
+					table.remove(entities, math.random(TableSize))
+					TableSize = TableSize - 1
+				end
+				for key, value in pairs(entities) do
+					value.set_command{
+						type = defines.command.compound,
+						structure_type = defines.compound_command.logical_and,
+						command = {
+							{
+								type = defines.command.go_to_location,
+								destination_entity = player,
+								distraction = defines.distraction.by_anything,
+								pathfind_flags = {
+									allow_destroy_friendly_entities = false,
+									allow_paths_through_own_entities = false,
+									cache = false,
+									prefer_straight_paths = false,
+									low_priority = false,
+									no_break = true
+								},
+								radius = 1
+							},
+							{
+								type = defines.command.attack,
+								target = player,
+								distraction = defines.distraction.by_damage
+							}
+						}
+					}
+				end
+			end
+		else
+			local player = game.get_player(command.player_index)
+			player.print("[Stream Integration] The Command " .. command.name .. " can only be used by the Stream Integration Mod!", player.chat_color)
+		end
+	else
+		local player = game.get_player(1)
+		player.print("[Stream Integration] The Stream Integration Mod is only available in Multiplayer!", player.chat_color)
 	end
 end)
---
+++ settings.lua
@@ -0,0 +1,98 @@
+data:extend({
+	{
+		type = "string-setting",
+		name = "ChannelName",
+		setting_type = "runtime-per-user",
+		order = "0",
+		allow_blank = true,
+		auto_trim = true,
+		default_value = "",
+		hidden = false
+	},
+	{
+		type = "bool-setting",
+		name = "SpawnPollutionEnable",
+		setting_type = "runtime-per-user",
+		order = "1",
+		default_value = false,
+		forced_value = false,
+		hidden = false
+	},
+	{
+		type = "int-setting",
+		name = "SpawnPollutionAmmount",
+		setting_type = "runtime-per-user",
+		order = "2",
+		auto_trim = true,
+		allowed_values = {10},
+		default_value = 10,
+		hidden = true
+	},
+	{
+		type = "bool-setting",
+		name = "TeleportEnable",
+		setting_type = "runtime-per-user",
+		order = "3",
+		default_value = false,
+		forced_value = false,
+		hidden = false
+	},
+	{
+		type = "int-setting",
+		name = "TeleportRadius",
+		setting_type = "runtime-per-user",
+		order = "4",
+		auto_trim = true,
+		allowed_values = {10},
+		default_value = 10,
+		hidden = true
+	},
+	{
+		type = "bool-setting",
+		name = "ParanoiaEnable",
+		setting_type = "runtime-per-user",
+		order = "5",
+		default_value = false,
+		forced_value = false,
+		hidden = false
+	},
+	{
+		type = "int-setting",
+		name = "ParanoiaRadius",
+		setting_type = "runtime-per-user",
+		order = "6",
+		auto_trim = true,
+		allowed_values = {100},
+		default_value = 100,
+		hidden = true
+	},
+	{
+		type = "int-setting",
+		name = "ParanoiaLimit",
+		setting_type = "runtime-per-user",
+		order = "7",
+		auto_trim = true,
+		allowed_values = {25},
+		default_value = 25,
+		hidden = true
+	},
+	{
+		type = "bool-setting",
+		name = "AttackEnable",
+		setting_type = "runtime-per-user",
+		order = "8",
+		default_value = false,
+		forced_value = false,
+		hidden = false
+	},
+	{
+		type = "int-setting",
+		name = "AttackLimit",
+		setting_type = "runtime-per-user",
+		order = "9",
+		auto_trim = true,
+		allowed_values = {25},
+		default_value = 25,
+		hidden = true
+	}
+})
